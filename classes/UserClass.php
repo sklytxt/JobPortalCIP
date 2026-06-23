9c7fd36832cb97927a7d7a9d69096013fa9504b3
sklytxt <minecraftme321@gmail.com>
2026-06-23 23:57:42 +0800

    added login and home

---
+++ classes/UserClass.php
@@ -0,0 +1,64 @@
+<?php
+class UserClass {
+    public $fullName, $email, $password, $location, $jobTitle, $contactNumber, $imagePath;
+    private static $databaseName = "jobdb";
+    private static $conn = null;
+
+    public function __construct($fullName = "", $email = "", $password = "", $location = "", $jobTitle = "", $contactNumber = "", $imagePath = "") {
+        $this->fullName = $fullName;
+        $this->email = $email;
+        $this->password = $password;
+        $this->location = $location;
+        $this->jobTitle = $jobTitle;
+        $this->contactNumber = $contactNumber;
+        $this->imagePath = $imagePath;
+    }
+
+    private static function getDb() {
+        if (self::$conn === null) {
+            self::$conn = new mysqli("localhost", "root", "", self::$databaseName);
+            if (self::$conn->connect_error) {
+                die("Connection failed: " . self::$conn->connect_error);
+            }
+        }
+        return self::$conn;
+    }
+
+    public static function getUserById($id) {
+        $conn = self::getDb();
+        $stmt = $conn->prepare("SELECT UserID, FullName, Email, JobTitle, Location, ContactNumber, ProfileImagePath, Usertype, CompanyName FROM users WHERE UserID = ? LIMIT 1");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $user = $stmt->get_result()->fetch_assoc();
+        if ($user) {
+            $user['DisplaySubtext'] = ($user['Usertype'] === 'employer') 
+                ? ($user['CompanyName'] ?? $user['JobTitle']) 
+                : ($user['JobTitle'] ?? '');
+        }
+        return $user;
+    }
+
+    public static function updateProfile($userId, $data, $file) {
+        $conn = self::getDb();
+        $fullName = $data['fullname'];
+        $email = $data['email'];
+        $jobTitle = $data['jobtitle'] ?? null;
+        $companyName = $data['companyname'] ?? null;
+        $contact = $data['contact'];
+        $location = $data['location'];
+        $fileName = null;
+        if (!empty($file['profile_image']['name'])) {
+            $fileName = time() . "_" . basename($file['profile_image']['name']);
+            move_uploaded_file($file['profile_image']['tmp_name'], "../uploads/" . $fileName);
+        }
+        if ($fileName) {
+            $stmt = $conn->prepare("UPDATE users SET FullName=?, Email=?, JobTitle=?, CompanyName=?, ContactNumber=?, Location=?, ProfileImagePath=? WHERE UserID=?");
+            $stmt->bind_param("sssssssi", $fullName, $email, $jobTitle, $companyName, $contact, $location, $fileName, $userId);
+        } else {
+            $stmt = $conn->prepare("UPDATE users SET FullName=?, Email=?, JobTitle=?, CompanyName=?, ContactNumber=?, Location=? WHERE UserID=?");
+            $stmt->bind_param("ssssssi", $fullName, $email, $jobTitle, $companyName, $contact, $location, $userId);
+        }
+        return $stmt->execute();
+    }
+}
+?>
